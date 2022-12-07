2bba09e0a3dac29fb52d4a251884ceb657ad59ca
Yael <yael.mattmann@gmail.com>
2022-12-07 12:26:41 +0000

    Added method Name and tests TestNamePass and TestNameFail

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -12,7 +12,18 @@ class EmployeeTest {
     }
 
     @Test
-
+    void TestNamePass(){
+        Employee myEmployee = new Employee("Jason","pps","phone","gender",0);
+        assertTrue(myEmployee.Name() > 5 || myEmployee.Name() < 22,"Name should be 5 to 22 characters");
+    }
+    @Test
+    void TestNameFail(){
+        Employee myEmployee = new Employee("Jazz","pps","phone","gender",0);
+        assertThrows(IllegalArgumentException.class,
+                () -> {myEmployee.Name();},
+                "Name should be 5 to 22 characters"
+        );
+    }
     @AfterEach
     void tearDown() {
     }
